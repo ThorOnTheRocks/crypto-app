0b14573a925c234c2d7dead30e46e6a28d6cd0f9
gianluca galota <gianluca.galota@gmail.com>
2022-09-12 18:26:43 +0100

    app layout created

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import './App.css';
-import { Switch, Route, Link } from 'react-router-dom';
+import { Routes, Route, Link } from 'react-router-dom';
 import { Layout, Typography, Space } from 'antd';
-import { Navbar } from './components';
+import { Navbar, Homepage, Exchanges, Cryptocurrencies, CryptoDetails, News } from './components';
+
+import 'antd/dist/antd.css'
+
 
 function App() {
   return (
@@ -10,10 +13,28 @@ function App() {
         <Navbar />
       </div>
       <div className='main'>
-
-      </div>
-      <div className='footer'>
-
+        <Layout>
+          <div className='routes'>
+            <Routes>
+              <Route exact path='/' element={<Homepage />} />
+              <Route exact path='/exchanges' element={<Exchanges />} />
+              <Route exact path='/cryptocurrencies' element={<Cryptocurrencies />} />
+              <Route exact path='/crypto/:cryptoId' element={<CryptoDetails />} />
+              <Route exact path='/news' element={<News />} />
+            </Routes>
+          </div>
+        </Layout>
+        <div className='footer' >
+          <Typography.Title level={5} style={{ color: 'white', textAlign: 'center' }}>
+            Cryptoverse <br />
+            All rights reserved
+          </Typography.Title>
+          <Space>
+            <Link to='/'>Home</Link>
+            <Link to='/exchanges'>Exchanges</Link>
+            <Link to='/news'>News</Link>
+          </Space>
+        </div>
       </div>
     </div>
   );
